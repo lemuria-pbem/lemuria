cb3b80b2250277fb21cf7a17dfc1819a51ce783b
lemuria-pbem <lemuria@online.de>
2021-01-16 16:40:39 +0100

    Refactoring.

--- src/Engine/Orders.php
+++ src/Engine/Orders.php
@@ -2,19 +2,19 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine;
 
-use Lemuria\Identifiable;
+use Lemuria\Id;
 
 interface Orders
 {
 	/**
 	 * Get the list of current orders for an entity.
 	 */
-	public function getCurrent(Identifiable $entity): Instructions;
+	public function getCurrent(Id $id): Instructions;
 
 	/**
 	 * Get the list of new default orders for an entity.
 	 */
-	public function getDefault(Identifiable $entity): Instructions;
+	public function getDefault(Id $id): Instructions;
 
 	/**
 	 * Load orders data.
