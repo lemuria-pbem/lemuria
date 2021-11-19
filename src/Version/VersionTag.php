d525b6268c19afbe4d156e80014f9c43787425ae
lemuria-pbem <lemuria@online.de>
2021-11-19 07:35:10 +0100

    Make VersionTag stringable.

--- src/Version/VersionTag.php
+++ src/Version/VersionTag.php
@@ -2,8 +2,12 @@
 declare(strict_types = 1);
 namespace Lemuria\Version;
 
-final class VersionTag
+final class VersionTag implements \Stringable
 {
 	public function __construct(public string $name, public string $version) {
+	}
+
+	public function __toString() {
+		return $this->name . ' ' . $this->version;
 	}
 }
