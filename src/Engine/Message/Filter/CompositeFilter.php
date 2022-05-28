5a34cc29db39c657fd83b32e435317293e0c2392
lemuria-pbem <lemuria@online.de>
2022-05-28 18:46:22 +0200

    Add a composite message filter.

---
+++ src/Engine/Message/Filter/CompositeFilter.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Engine\Message\Filter;
+
+use Lemuria\Engine\Message;
+use Lemuria\Engine\Message\Filter;
+
+/**
+ * This filter allows composition of multiple filters.
+ */
+class CompositeFilter implements Filter
+{
+	/**
+	 * @var Filter[]
+	 */
+	protected array $filters = [];
+
+	public function retains(Message $message): bool {
+		foreach ($this->filters as $filter) {
+			if ($filter->retains($message)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function add(Filter $filter): CompositeFilter {
+		$this->filters[] = $filter;
+		return $this;
+	}
+}
