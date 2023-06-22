2730d83d616e82eacb40f1d7150bd9020acf3549
lemuria-pbem <lemuria@online.de>
2023-06-22 15:43:13 +0200

    [FEATURE] Add opposite direction method.

--- src/Model/World/Direction.php
+++ src/Model/World/Direction.php
@@ -8,6 +8,23 @@ enum Direction : string
 
 	public final const ROUTE_STOP = self::None;
 
+	/**
+	 * Get the opposite direction.
+	 */
+	public function getOpposite(): Direction {
+		return match ($this) {
+			Direction::Northeast => Direction::Southwest,
+			Direction::East      => Direction::West,
+			Direction::Southeast => Direction::Northwest,
+			Direction::Southwest => Direction::Northeast,
+			Direction::West      => Direction::East,
+			Direction::Northwest => Direction::Southeast,
+			Direction::North     => Direction::South,
+			Direction::South     => Direction::North,
+			default              => Direction::None
+		};
+	}
+
 	case None = '';
 
 	case North = 'N';
